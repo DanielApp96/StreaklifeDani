1bf1e50dd0d532a33503bd2f7857e1ebec100262
DanielApp96 <daniel.hadzhiangelov@gmail.com>
2026-03-18 09:51:51 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,1 @@
-// App.js
-import React from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { HabitProvider } from './src/context/HabitContext';
-import AppNavigator from './src/navigation/AppNavigator';
 
-export default function App() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <HabitProvider>
-        <AppNavigator />
-      </HabitProvider>
-    </GestureHandlerRootView>
-  );
-}
